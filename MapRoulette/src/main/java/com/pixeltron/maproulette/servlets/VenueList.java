951a18b04fd0ec6e0bc1744ff3c99b25b442798d
Rainer Lim <limrainer.ph@gmail.com>
2021-04-08 00:24:59 -0600

    Add more code for factory pattern

--- MapRoulette/src/main/java/com/pixeltron/maproulette/servlets/VenueList.java
+++ MapRoulette/src/main/java/com/pixeltron/maproulette/servlets/VenueList.java
@@ -1,5 +1,9 @@
 package com.pixeltron.maproulette.servlets;
 
+import java.util.List;
+
+import fi.foyt.foursquare.api.entities.CompactVenue;
+
 public abstract class VenueList {
     protected abstract List<CompactVenue> createVenueList();
 }
--- maproulette/src/main/java/com/pixeltron/maproulette/servlets/RouletteServlet.java
+++ maproulette/src/main/java/com/pixeltron/maproulette/servlets/RouletteServlet.java
@@ -135,56 +135,12 @@ public class RouletteServlet extends HttpServlet {
                     curWP.lng = BigDecimal.valueOf(lng);
                     waypoints.add(curWP);
                 }
-                
-                List<Future<HTTPResponse>> responses = Lists.newArrayList();
-                for (LatLng waypoint : waypoints) {
-                    fetch = URLFetchServiceFactory.getURLFetchService();
-                    parameters.waypoint = waypoint;
-                    responses.add(fetch.fetchAsync(new URL(request_maker.makeFoursquareRequest(parameters))));
-                }
-                
-                List<RecommendationGroup> foursquareResults = Lists.newArrayList();
-                for (Future<HTTPResponse> futureFsqresp : responses) {
-                	try {
-	                	HTTPResponse fsqresp = futureFsqresp.get();
-	                	FoursquareApiRequestResponse response = handleApiResponse(
-			                			new Response(new String(fsqresp.getContent(), "UTF-8"), 
-			                			fsqresp.getResponseCode(), 
-			                			null));
-	             	
-	                    if (response.getMeta().getCode() == 200) {
-							RecommendationGroup[] groups = (RecommendationGroup[]) JSONFieldParser.parseEntities(
-											RecommendationGroup.class, 
-											response.getResponse().getJSONArray("groups"), 
-											true);
-							if (groups.length > 0) {
-								if (groups[0].getItems().length > 0)
-									foursquareResults.add(groups[0]);
-							}
-	                    	
-	                    }
-                	} catch (Exception e) {
-                		e.printStackTrace();
-                	}
-                }
-                
-                List<CompactVenue> venueResults = Lists.newArrayList();
-                for (RecommendationGroup result : foursquareResults) {
-                		Recommendation[] venues = result.getItems();
-                		List<CompactVenue> venueData = Lists.newArrayList();
-                		for (Recommendation venue : venues) {
-                			venueData.add(venue.getVenue());
-                		}
-                		int random = rand.nextInt(venues.length);
-                		CompactVenue currentVenue = venueData.get(random);
-                		venueData.remove(random);
-                		while (venueResults.contains(currentVenue) && !venueData.isEmpty()) {
-                			random = rand.nextInt(venueData.size());
-                			currentVenue = venueData.get(random);
-                			venueData.remove(random);
-                		}
-                		venueResults.add(currentVenue);
-                }
+
+				List<CompactVenue> venueResults = Lists.newArrayList();
+
+                VenueListFactory venueListFactory = new VenueListFactory();
+				VenueList venueList = venueListFactory.getVenueList("FOURSQUARE");
+				venueResults = venueList.createVenueList();
                 
                 if (venueResults.size() > 0) {
                 	wayResp.setData(venueResults);
