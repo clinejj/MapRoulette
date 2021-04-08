0059df2bb25e6eca41635d5c57c8fbf0682665d3
Rainer Lim <limrainer.ph@gmail.com>
2021-04-08 01:20:41 -0600

    Delete files from wrong directory

--- MapRoulette/src/main/java/com/pixeltron/mapquest/open/geocoding/Request.java
+++
@@ -1,7 +0,0 @@
-package com.pixeltron.mapquest.open.geocoding;
-
-import java.io.UnsupportedEncodingException;
-
-public interface Request {
-    String buildUrl(RequestParameters parameters) throws UnsupportedEncodingException;
-}
